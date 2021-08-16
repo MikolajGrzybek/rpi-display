293eb56de37fee76316c186ee485db36569fce33
Mikolaj <grzybek.mikolaj@gmail.com>
2021-08-16 23:27:50 +0200

    .

--- draw_gui/draw_gui.cpp
+++ draw_gui/draw_gui.cpp
@@ -222,7 +222,7 @@ UBYTE Dynamic_Refresh(UWORD width, UWORD height, UWORD start_x, UWORD start_y, i
 
   
     /* DRAW HERE */
-    Paint_DrawNum(Area.Dynamic_Area_Width/2, Area.Dynamic_Area_Height/2, msg, &Font24, 0x00, 0xF0);     
+    Paint_DrawNum(Area.Dynamic_Area_Width/2, Area.Dynamic_Area_Height/2, msg, &Font12, 0x00, 0xF0);     
 
 
     EPD_IT8951_1bp_Refresh(Refresh_Frame_Buf, Area.Start_X, Area.Start_Y, width, height, A2_Mode, Init_Target_Memory_Addr, true);
